5e095cba02c6089deb3bdf4f2920b7f359cda5b9
Samuel Johansson <samuel.johansson@aalto.fi>
2019-08-07 10:56:12 +0300

    drawStringData valmis

--- src/drawplayer.py
+++ src/drawplayer.py
@@ -7,6 +7,10 @@ class DrawPlayer():
 		self.start_xPos = start_xPos
 		self.start_yPos = start_yPos
 		
+		self.number = 0
+		self.hearth = 0
+		self.breath = 0
+		
 		pygame.draw.line(self.surface, pygame.Color(155,155,155), (start_xPos, start_yPos), (start_xPos+500, start_yPos))
 		pygame.draw.line(self.surface, pygame.Color(155,155,155), (start_xPos, start_yPos+100), (start_xPos+500, start_yPos+100))
 		pygame.draw.line(self.surface, pygame.Color(155,155,155), (start_xPos, start_yPos), (start_xPos+100, start_yPos))
@@ -16,5 +20,11 @@ class DrawPlayer():
 	def drawStringData(self, name, hearth, breath):
 		pygame.font.init()
 		font = pygame.font.SysFont('Calibri', 30)
-		textsurface = font.render('#'+str(name), False, (155, 155, 155))
-		self.surface.blit(textsurface, (10, self.start_yPos+10))
+		self.number = font.render('#'+str(name), False, (155, 155, 155))
+		self.surface.blit(self.number, (0, self.start_yPos))
+		
+		self.hearth = font.render('Hearth:'+str(hearth), False, (155, 155, 155))
+		self.surface.blit(self.hearth, (0, self.start_yPos+50))
+		
+		self.breath = font.render('Breath:'+str(breath), False, (155, 155, 155))
+		self.surface.blit(self.breath, (125, self.start_yPos+50))
