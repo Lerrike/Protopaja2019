1a4750192cab33156f50148638e631288226e230
Samuel Johansson <samuel.johansson@aalto.fi>
2019-08-07 10:45:26 +0300

    Lisää piirtoa.

--- src/drawplayer.py
+++ src/drawplayer.py
@@ -12,3 +12,9 @@ class DrawPlayer():
 		pygame.draw.line(self.surface, pygame.Color(155,155,155), (start_xPos, start_yPos), (start_xPos+100, start_yPos))
 		pygame.draw.line(self.surface, pygame.Color(155,155,155), (start_xPos+500, start_yPos), (start_xPos+500, start_yPos+100))
 		pygame.draw.line(self.surface, pygame.Color(155,155,155), (start_xPos+250, start_yPos), (start_xPos+250, start_yPos+100))
+		
+	def drawStringData(self, name, hearth, breath):
+		pygame.font.init()
+		font = pygame.font.SysFont('Calibri', 30)
+		textsurface = font.render('#'+str(name), False, (155, 155, 155))
+		self.surface.blit(textsurface, (10, self.start_yPos+10))
--- src/player.py
+++ src/player.py
@@ -8,44 +8,44 @@ class Player():
 		self.xPos = 0
 		self.yPos = 0
 		
-	def update_all(new_hearth, new_breath, new_xPos, new_yPos):
+	def update_all(self, new_hearth, new_breath, new_xPos, new_yPos):
 		pass
 		
-	def return_number():
+	def return_number(self):
 		return self.number
 		
-	def change_hearth(new_hearth):
+	def change_hearth(self, new_hearth):
 		self.hearth = new_hearth
 	
-	def return_hearth():
+	def return_hearth(self):
 		return self.hearth
 		
-	def update_hearthList(new_hearth):
+	def update_hearthList(self, new_hearth):
 		pass
 		
-	def return_hearthList():
+	def return_hearthList(self):
 		return self.hearthList
 		
-	def change_breath(new_breath):
+	def change_breath(self, new_breath):
 		self.breath = new_breath
 	
-	def return_breath():
+	def return_breath(self):
 		return self.breath
 		
-	def update_breathList(new_breath):
+	def update_breathList(self, new_breath):
 		pass
 		
-	def return_breathList():
+	def return_breathList(self):
 		return self.breathList
 		
-	def change_xPos(new_xPos):
+	def change_xPos(self, new_xPos):
 		self.xPos = new_xPos
 		
-	def return_xPos():
+	def return_xPos(self):
 		return self.xPos
 		
-	def change_yPos(new_yPos):
+	def change_yPos(self, new_yPos):
 		self.yPos = new_yPos
 		
-	def return_yPos():
+	def return_yPos(self):
 		return self.yPos
