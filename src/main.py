682f420f14e8830d191c4e2cafc0a063660397a2
tiiamariah <tiia.hyvonen@aalto.fi>
2019-08-07 10:00:48 +0300

    valkea tausta

--- src/main.py
+++ src/main.py
@@ -3,15 +3,19 @@ from pygame.locals import *
 
 def main():
 	pygame.init()
-	surface = pygame.display.set_mode((1000,500))
+	surface = pygame.display.set_mode((750,500))
+	white = [255, 255, 255]
+	surface.fill(white)
 	pygame.display.set_caption('Aboense')
 	
+	pygame.display.flip()
 	while True:
 		time.sleep(.01)
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
+
 
 if __name__ == '__main__':
 	main()
