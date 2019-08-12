e79d814b689514dba6b2147bc6c6523ae97b5fb8
tiiamariah <tiia.hyvonen@aalto.fi>
2019-08-12 08:56:28 +0300

    datan parsaus

---
+++ src/Data_parse.py
@@ -0,0 +1,33 @@
+def data_parse(line): #example line: 26.7.2019.11.10.54#x/10#y/5#Breathing/33#Heart/85
+    data_list = [0,0,0,0] #[x, y, breathing, heart]
+    
+    for i in range (0, len(line)):
+        if line[i] == 'x':
+            if line[i+3].isdigit() == True:
+                data_list[0] = line[(i+2):(i+4)]
+            else:
+                data_list[0] = line[i+2]
+                
+        if line[i] == 'y':
+            if line[i+3].isdigit() == True:
+                data_list[1] = line[(i+2):(i+4)]
+            else:
+                data_list[1] = line[i+2]
+                
+        if line[i] == 'g':
+            if line[i+3].isdigit() == True:
+                data_list[2] = line[(i+2):(i+4)]
+            else:
+                data_list[2] = line[i+2]
+        
+        if line[i] == 'H':
+            if len(line)-(i+9) == 0:
+                data_list[3] = line[(i+6):(i+9)]
+            else:
+                data_list[3] = line[(i+6):(i+8)]
+         
+        
+def main():
+    data_parse("26.7.2019.11.10.54#x/1#y/54#Breathing/33#Heart/85")
+
+main()
