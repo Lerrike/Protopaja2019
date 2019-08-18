4bf597ac0937d9f2a411e0e82d0b5c589d0d9ef9
Samuel Johansson <samuel.johansson@aalto.fi>
2019-08-18 21:07:33 +0300

    Korjaus parseen.

--- src/Data_parse.py
+++ src/Data_parse.py
@@ -2,7 +2,8 @@
 def data_parse(line): #example line: Nimi/Samuel#26.7.2019.11.10.54#x/10#y/5#Breathing/33#Heart/85
     data_list = [0,0,0,0,0] #[x, y, breathing, heart]
     parts = line.split("#")
-    data_list[0] = parts[0].split("/")[1]
+    print(line)
+    data_list[0] = parts[1].split("/")[1]
     data_list[1] = parts[2].split("/")[1]
     data_list[2] = parts[3].split("/")[1]
     data_list[3] = parts[4].split("/")[1]
